7e0a2c5cedfcc749e4df348b41940c9b03dc2a3f
LuRobot15 <thelukeroberts@gmail.com>
2024-02-26 18:45:53 +0000

    added more validation to views

--- cw1/coursework1_project/newsAgency/views.py
+++ cw1/coursework1_project/newsAgency/views.py
@@ -38,7 +38,85 @@ def stories(request):
 	return render(request, 'stories.html')
 
 def post_story(request):
-	return render(request, 'post_story.html')
+    #checking if the user is logged in
+	if not request.user.is_authenticated:
+		http_response = HttpResponse("Not logged in", content_type='text/plain')
+		http_response.status_code = 503
+		http_response.reason_phrase = 'Service Unavailable'
+		return http_response
+	#checking if the user is an author
+	try:
+		author = Author.objects.get(user=request.user)
+	except Author.DoesNotExist:
+		#creating an author object for the user
+		author = Author.objects.create(user=request.user)
+	
+	#creating the article object
+	story_headline = request.POST.get("headline")
+	story_catagory = request.POST.get("story_catagory")
+	story_region = request.POST.get("story_region")
+	story_details = request.POST.get("story_details")
+
+	#validating params given
+	if story_headline == None:
+		http_response = HttpResponse("You must supply a headline", content_type='text/plain')
+		http_response.status_code = 503
+		http_response.reason_phrase = 'Service Unavailable'
+		return http_response
+	if story_catagory == None:
+		http_response = HttpResponse("You must supply a story catagory", content_type='text/plain')
+		http_response.status_code = 503
+		http_response.reason_phrase = 'Service Unavailable'
+		return http_response
+	if story_region == None:
+		http_response = HttpResponse("You must supply a story region", content_type='text/plain')
+		http_response.status_code = 503
+		http_response.reason_phrase = 'Service Unavailable'
+		return http_response
+	if story_details == None:
+		http_response = HttpResponse("You must supply story details", content_type='text/plain')
+		http_response.status_code = 503
+		http_response.reason_phrase = 'Service Unavailable'
+		return http_response
+
+	#validating the story catagory
+	if story_catagory not in ['pol', 'art', 'tech', 'trivia']:
+		http_response = HttpResponse("Invalid story catagory", content_type='text/plain')
+		http_response.status_code = 503
+		http_response.reason_phrase = 'Service Unavailable'
+		return http_response
+
+	#validating the story region
+	if story_region not in ['uk', 'eu', 'w']:
+		http_response = HttpResponse("Invalid story region", content_type='text/plain')
+		http_response.status_code = 503
+		http_response.reason_phrase = 'Service Unavailable'
+		return http_response
+
+	#validating the story details
+	if len(story_details) > 128:
+		http_response = HttpResponse("Story details too long", content_type='text/plain')
+		http_response.status_code = 503
+		http_response.reason_phrase = 'Service Unavailable'
+		return http_response
+
+	#validating the story headline
+	if len(story_headline) > 64:
+		http_response = HttpResponse("Story headline too long", content_type='text/plain')
+		http_response.status_code = 503
+		http_response.reason_phrase = 'Service Unavailable'
+		return http_response
+
+	#creating the article object
+	Article.objects.create(
+		headline=story_headline,
+		story_catagory=story_catagory,
+		region=story_region,
+		author=author,
+		story_details=story_details
+	)
+
+	return HttpResponse(status=201, reason='Created')
 
 def get_stories(request):
 	story_catagory = request.GET.get("story_cat")
@@ -47,11 +125,11 @@ def get_stories(request):
  
 	stories = Article.objects.all()
 
-	if story_catagory != "*":
+	if story_catagory != "*" and story_catagory != None:
 		stories = stories.filter(story_catagory=story_catagory)
-	if story_region != "*":
+	if story_region != "*" and story_region != None:
 		stories = stories.filter(region=story_region)
-	if story_date != "":
+	if story_date != "" and story_date != None:
 		stories = stories.filter(created_at__gte=story_date)
   
 	story_collection = []
@@ -68,6 +146,7 @@ def get_stories(request):
 			"story_details" : story.story_details
 		}
 		story_collection.append(item)
+  
 	if (len(story_collection) == 0):
 		http_response = HttpResponse("No stories found matching the given criteria", content_type='text/plain')
 		http_response.status_code = 404
